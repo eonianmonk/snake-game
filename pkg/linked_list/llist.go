74f22dd059d0080fa7a07093b094b86c7f9bb673
eonianmonk <berezhnenko.dmitry@gmail.com>
2024-02-07 15:39:02 +0200

    added deletion to linked list

--- pkg/linked_list/llist.go
+++ pkg/linked_list/llist.go
@@ -1,5 +1,7 @@
 package linkedlist
 
+import "fmt"
+
 type Node[T any] struct {
 	data T
 	next *Node[T]
@@ -67,7 +69,7 @@ func (ll *LinkedList[T]) Contains(value T, compFn func(T, T) bool) bool {
 // returns node at certain index.
 // positive integers to start at head
 // negative to start at end
-func (ll *LinkedList[T]) At(ix int) *Node[T] {
+func (ll *LinkedList[T]) at(ix int) *Node[T] {
 	pos := 0
 	var ptr *Node[T]
 	if ix >= 0 {
@@ -84,4 +86,36 @@ func (ll *LinkedList[T]) At(ix int) *Node[T] {
 		}
 	}
 	return ptr
+}
+
+// returns node at certain index.
+// positive integers to start at head
+// negative to start at end
+func (ll *LinkedList[T]) At(ix int) *Node[T] {
+	return ll.at(ix)
+}
+
+// as in At(..) function - positive index means starting from head
+// and negative - from end
+func (ll *LinkedList[T]) DeleteAt(ix int) error {
+	node := ll.at(ix)
+	if node == nil {
+		return fmt.Errorf("no node at index %d", ix)
+	}
+	if node.next != nil {
+		node.next.prev = node.prev
+	}
+	if node.prev != nil {
+		node.prev.next = node.next
+	}
+	ll.length--
+	return nil
+}
+
+func (ll *LinkedList[T]) Slice() []T {
+	slice := make([]T, ll.length)
+	for i := 0; i < ll.length; i++ {
+		slice = append(slice, ll.at(i).Value())
+	}
+	return slice
 }
