840d3921e73f62924ec08f50328fca3c5d7757b4
eonianmonk <berezhnenko.dmitry@gmail.com>
2024-02-08 19:32:10 +0200

    fixed grid layout

--- layout/game_grid.go
+++ layout/game_grid.go
@@ -9,50 +9,36 @@ import (
 type GameGrid struct {
 	*tview.Grid
 	cells [][]*tview.Box
-	game  *game.Game
 }
 
-func (gg *GameGrid) Draw(screen tcell.Screen) {
-	for i := range gg.cells {
-		for j := range gg.cells {
-			gg.cells[i][j].Draw(screen)
-		}
-	}
-}
+func NewGameGrid(size int) *GameGrid {
 
-func NewGameGrid(size int, game *game.Game) *GameGrid {
-
-	gridPlaceholder := make([]int, size)
-	for i := range gridPlaceholder {
-		gridPlaceholder[i] = -1
+	ph := make([]int, size)
+	for i := range ph {
+		ph[i] = -1
 	}
 
 	grid := tview.NewGrid().
-		SetRows(gridPlaceholder...).
-		SetColumns(gridPlaceholder...)
+		SetRows(ph...).
+		SetColumns(ph...)
 
 	cells := make([][]*tview.Box, size)
 	for i := range cells {
 		cells[i] = make([]*tview.Box, size)
 		for j := range cells[i] {
 
-			cell := tview.NewBox().
-				SetBackgroundColor(tcell.ColorGray)
-				//SetBorder(true).SetBorderAttributes(tcell.AttrBold).
-				//SetTitle(fmt.Sprintf("%d %d", i, j))
-			if (i+j)%2 == 0 {
-				cell.SetBackgroundColor(tcell.ColorOlive)
-			}
+			cell := tview.NewBox()
+
+			cell.SetBackgroundColor(BlankColor)
 
 			cells[i][j] = cell
-			grid.AddItem(cell, i, j, 1, 1, 1, 1, false)
+			grid.AddItem(cell, i, j, 1, 1, 0, 0, false)
 		}
 	}
 
 	return &GameGrid{
 		Grid:  grid,
 		cells: cells,
-		game:  game,
 	}
 }
 
